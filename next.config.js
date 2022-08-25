1a75db5e4922ddf5082a321faee51dbacfea9179
luccas <luccasmtxr@gmail.com>
2022-08-25 15:20:53 -0300

    folder estructure

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    loader: "custom"    
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  }
 }
 
 module.exports = nextConfig
