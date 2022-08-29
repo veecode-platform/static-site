40a2b126deb1d112cbd9b88adfd7b0c29541ce7f
ValberJunior <valberjr1992@gmail.com>
2022-08-29 15:25:20 -0300

    create index for screens

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,6 +1,1 @@
 export { default as HomeScreen } from "./HomeScreen/HomeScreen";
-export { default as TalentosScreen} from "./TalentosScreen/TalentosScreen"
-export { default as ForBusinesses } from "./ForBusinesses/ForBusinesses";
-export { default as AboutScreen } from "./AboutScreen/AboutScreen";
-export { default as ContactScreen } from "./ContactScreen/ContactScreen";
-export { default as NivelamentoScreen} from "./NivelamentoScreen/NivelamentoScreen";
