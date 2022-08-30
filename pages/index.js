19c13ea493071eb016527bd3b2cb45a0319981f5
ValberJunior <valberjr1992@gmail.com>
2022-08-29 22:50:10 -0300

    change title in head -- homepage

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Platform | Home</title>
+        <title>Platform  |  Homepage </title>
         <meta name="description" content="description" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
