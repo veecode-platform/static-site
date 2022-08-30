ce148a443698decfee6e466050f8e3a06d2119e6
ValberJunior <valberjr1992@gmail.com>
2022-08-29 22:37:02 -0300

    import benefits component

--- src/screens/homescreen/Homescreen.jsx
+++ src/screens/homescreen/Homescreen.jsx
@@ -1,12 +1,14 @@
 import style from "../../../styles/_PageDefault.module.scss";
 import { Header } from '../../components';
 import FirstSection from "./firstSection";
+import Benefits from "./benefits";
 
 const Homescreen = () => {
   return (
     <main className={style.container}>
       <Header/>
       <FirstSection/>
+      <Benefits/>
     </main>
   )
 }
