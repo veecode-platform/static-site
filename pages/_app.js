c84be10b958787b7d992b8ec69ceff90cb1a7504
ValberJunior <valberjr1992@gmail.com>
2022-09-06 11:53:08 -0300

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+import Script from "next/script";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import "../styles/theme.scss";
@@ -5,6 +7,15 @@ import "../styles/theme.scss";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    {/* Global site tag (gtag.js) - Google Analytics */}
+    <Script src={"https://www.googletagmanager.com/gtag/js?id=G-VH5ZB0M4YZ%22%3E"}></Script>
+      <Script id="google-tag-script-function">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-VH5ZB0M4YZ');`}
+      </Script>
       <Component {...pageProps} />
       <ToastContainer
         autoClose={5000}
