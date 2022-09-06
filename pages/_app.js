48e142b79127f4d4e7ab35464a4840bfb14b2d30
ValberJunior <valberjr1992@gmail.com>
2022-09-06 11:53:51 -0300

    add hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,17 @@ function MyApp({ Component, pageProps }) {
 
         gtag('config', 'G-VH5ZB0M4YZ');`}
       </Script>
+      {/*Hotjar Tracking Code for https://platform.vee.codes/ */}
+      { <Script>
+        {` (function(h,o,t,j,a,r){
+        h.hj=h.hjfunction(){(h.hj.q=h.hj.q[]).push(arguments)};
+        h._hjSettings={hjid:3144537,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-%27,%27.js?sv=%27);`}
+      </Script> }
       <Component {...pageProps} />
       <ToastContainer
         autoClose={5000}
