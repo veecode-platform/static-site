7d4d7bb5f0676bc9afcbaa574e19cc5cb876ebee
ValberJunior <valberjr1992@gmail.com>
2022-09-06 14:45:18 -0300

    add dynamic imports

--- src/screens/homescreen/Homescreen.jsx
+++ src/screens/homescreen/Homescreen.jsx
@@ -1,9 +1,13 @@
 import style from "../../../styles/_Homescreen.module.scss";
 import { Header } from '../../components';
 import FirstSection from "./firstSection";
-import Benefits from "./benefits";
-import OptimizationAndSupport from "./optimizationAndSupport";
-import ContactSection from "./contactSection";
+// import Benefits from "./benefits";
+// import OptimizationAndSupport from "./optimizationAndSupport";
+// import ContactSection from "./contactSection";
+import dynamic from "next/dynamic";
+const Benefits = dynamic (()=> import ('./benefits'));
+const OptimizationAndSupport = dynamic (()=> import ('./optimizationAndSupport'));
+const ContactSection = dynamic (()=> import ('./contactSection'));
 
 const Homescreen = () => {
   return (
