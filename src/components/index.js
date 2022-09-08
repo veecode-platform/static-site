3b8f8baea1f0ab94844e8c92304ce87186c9af54
ValberJunior <valberjr1992@gmail.com>
2022-09-08 18:43:01 -0300

    create ContactBar component

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export {default as Paypal } from './paypal/PaypalComponent';
 export { default as Breadcrumb } from './breadcrumb/Breadcrumb';
 export { default as Footer} from './footer/Footer';
 export { default as DefaultPage } from './defaultPage/DefaultPage';
+export { default as ContactBar } from './contactBar/ContactBar';
