e4ae19b949362e092c2b643692b0857c10860404
ValberJunior <valberjr1992@gmail.com>
2022-09-08 12:37:28 -0300

    add default page component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Button } from './button/Button';
 export {default as Paypal } from './paypal/PaypalComponent';
 export { default as Breadcrumb } from './breadcrumb/Breadcrumb';
 export { default as Footer} from './footer/Footer';
+export { default as DefaultPage } from './defaultPage/DefaultPage';
