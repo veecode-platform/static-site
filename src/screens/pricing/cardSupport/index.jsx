730db7a3b48ae2f9d0c50d95ba1b62427dc6a46c
ValberJunior <valberjr1992@gmail.com>
2022-09-08 08:58:45 -0300

    rename screen

---
+++ src/screens/pricing/cardSupport/index.jsx
@@ -0,0 +1,37 @@
+import style from './_CardSupport.module.scss';
+
+const CardSupport = ({
+    title,
+    subtitle,
+    desc,
+    children,
+    price
+}) => {
+  return (
+    <div className={style.content}>
+        <div className={style.content__text}>
+            <div className={style["content__text-title"]}>
+                <h3>{title}</h3>
+            </div>
+            <div className={style["content__text-subtitle"]}>
+                <h4>{subtitle}</h4>
+            </div>
+            <span/>
+            <div className={style["content__text-desc"]}>
+                <p>
+                    {desc}
+                </p>
+            </div>
+            <span/>
+            <div className={style["content__text-price"]}>
+                <p>
+                    {price}
+                </p>
+            </div>
+            { children !== "" | children !== null ?  children :  null }
+        </div>
+    </div>
+  )
+}
+
+export default CardSupport
