99b398acf4d7e074193e07a16d4e0724bd2601e9
ValberJunior <valberjr1992@gmail.com>
2022-09-10 11:10:44 -0300

    code review -- style changes -- responsive version

--- src/screens/homescreen/firstSection/index.jsx
+++ src/screens/homescreen/firstSection/index.jsx
@@ -4,10 +4,17 @@ import Link from "next/link";
 import TextSlide from "./textSlide";
 
 const Logo = "assets/home/logo.png";
+const BgDesktop = "assets/home/bg1.png"; 
+const BgMobile = "assets/home/bgmobile1.png";
 
 const FirstSection = () => {
   return (
     <section className={style.wrapper}>
+      {/* Banner Desktop */}
+      <div className={style.wrapper__background}>
+        <img src={BgDesktop} alt="background header - Desktop version" className={style["wrapper__background-desktop"]} />
+        <img src={BgMobile} alt="background header - Desktop version" className={style["wrapper__background-mobile"]} />
+      </div>
       <article className={style.content}>
         <img
           src={Logo}
@@ -17,14 +24,16 @@ const FirstSection = () => {
         <div className={style.content__title}>
           <h1>
             Your API infrastructure in <strong> minutes </strong>
-            <TextSlide/>
+            <TextSlide />
           </h1>
         </div>
         <div className={style.content__text}>
           <p>
-          Your transition to the cloud doesn&lsquo;t have to be time-consuming, expensive or unaided. Count
-          on our <strong>professional support</strong> and <strong>free tools</strong> to overcome these challenges quickly and
-          easily.
+            Your transition to the cloud doesn&lsquo;t have to be
+            time-consuming, expensive or unaided. Count on our{" "}
+            <strong>professional support</strong> and{" "}
+            <strong>free tools</strong> to overcome these challenges quickly and
+            easily.
           </p>
         </div>
         <div className={style.content__buttons}>
@@ -34,10 +43,7 @@ const FirstSection = () => {
             </a>
           </Link> */}
 
-          <Link
-            href="/pricing"
-            passHref
-          >
+          <Link href="/pricing" passHref>
             <a>
               <Button>Buy Now</Button>
             </a>
