805880796eb88c57fe19de67aa1cd57f916fcdd3
ValberJunior <valberjr1992@gmail.com>
2022-09-11 18:11:11 -0300

    code review

--- src/screens/support/listDetails/index.jsx
+++ src/screens/support/listDetails/index.jsx
@@ -7,18 +7,42 @@ const ListDetails = () => {
         <h3 className={style.title}>Our experts are ready to help you with</h3>
         <div className={style.details}>
             <ul className={style.details__list}>
-                <li className={style["details__list-item"]}> <span><FaCheck/></span> Kong API Gateway</li>
-                <li className={style["details__list-item"]}> <span><FaCheck/></span> Kubernetes</li>
-                <li className={style["details__list-item"]}> <span><FaCheck/></span> Aws eks</li>
-                <li className={style["details__list-item"]}> <span><FaCheck/></span> Mockserver</li>
-                <li className={style["details__list-item"]}> <span><FaCheck/></span> Okta</li>
-                <li className={style["details__list-item"]}> <span><FaCheck/></span> Dev Portal</li>
-                <li className={style["details__list-item"]}> <span><FaCheck/></span> Insomnia</li>
-                <li className={style["details__list-item"]}> <span><FaCheck/></span> Okteto</li>
-                <li className={style["details__list-item"]}> <span><FaCheck/></span> Git Providers</li>
-                <li className={style["details__list-item"]}> <span><FaCheck/></span> OpenAPI</li>
-                <li className={style["details__list-item"]}> <span><FaCheck/></span> Code Generator Quarkus</li>
-                <li className={style["details__list-item"]}> <span><FaCheck/></span> Code Generator Spring Boot</li>
+                <li className={style["details__list-item"]}> 
+                  <span><FaCheck/></span> Kong API Gateway
+                </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> Kubernetes
+                </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> Aws eks
+                </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> Mockserver
+                </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> Okta
+                </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> Dev Portal
+                </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> Insomnia
+                </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> Okteto
+                </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> Git Providers
+                </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> OpenAPI
+                </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> Code Generator Quarkus
+                </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> Code Generator Spring Boot
+                </li>
             </ul>
         </div>
     </article>
