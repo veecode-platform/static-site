5c61952fca9d4c38e7d2497b256546f77235643d
ValberJunior <valberjr1992@gmail.com>
2022-09-11 13:44:00 -0300

    code review

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,7 +3,10 @@ import style from '../../../styles/_Button.module.scss';
 const Button = ({color,background,children}) => {
   return (
     <button 
-       style={{color:color,background:background}}
+       style={{
+        color:color,
+        background:background
+        }}
        className={style.button}>
         {children}
     </button>
