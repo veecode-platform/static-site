141e97068b7d1ac8ba08597c3e73dc9873bc94f1
ValberJunior <valberjr1992@gmail.com>
2022-09-12 20:55:52 -0300

    code review

--- src/screens/checkout/Checkout.jsx
+++ src/screens/checkout/Checkout.jsx
@@ -18,29 +18,29 @@ const Checkout = () => {
         <article className={style.content}>
           <Breadcrumb active={3} />
           <div className={style.content__options}>
-            <div className={style.content__options_infoBoxWrapper}>
-              <div className={style.content__options_infoBoxWrapper_box}>
+            <div className={style["content__options-infoBoxWrapper"]}>
+              <div className={style.box}>
                 <h1>Order summary</h1>
               </div>
-              <div className={style.content__options_infoBoxWrapper_info}>
-                <div className={style.content__options_infoBoxWrapper_info_left} >
+              <div className={style.info}>
+                <div className={style.info__left}>
                   <p>Support plan: Premium</p>
                   <p>SLA</p>
                 </div>
-                <div className={style.content__options_infoBoxWrapper_info_right}>
+                <div className={style.info__right}>
                   <p>$749/mo</p>
                   <p>3 days</p>
                 </div>
               </div>
 
-              <div className={style.content__options_infoBoxWrapper_info}>
-                <div className={style.content__options_infoBoxWrapper_info_left}>
+              <div className={style.info}>
+                <div className={style.info__left}>
                   <p>20 users</p>
                   <p>Devportal</p>
                   <p>Safira-cli</p>
                   <p>VKPR</p>
                 </div>
-                <div className={style.content__options_infoBoxWrapper_info_right}>
+                <div className={style.info__right}>
                   <p>$0/mo</p>
                   <p>$0/mo</p>
                   <p>$0/mo</p>
@@ -48,15 +48,15 @@ const Checkout = () => {
                 </div>
               </div>
 
-              <div className={style.content__options_infoBoxWrapper_info}>
-                <div className={style.content__options_infoBoxWrapper_info_left}>
+              <div className={style.info}>
+                <div className={style.info__left}>
                   <p>Due today:</p>
                   <p style={{ marginBottom: "1em" }}>Due monthly:</p>
                   <p>
                     Billed Monthly on the <strong>{getDateFormatted()}</strong>
                   </p>
                 </div>
-                <div className={style.content__options_infoBoxWrapper_info_right}>
+                <div className={style.info__right}>
                   <p>$749/mo</p>
                   <p>
                     <strong>$749/mo</strong>
@@ -64,30 +64,22 @@ const Checkout = () => {
                 </div>
               </div>
 
-              <div className={style.content__options_infoBoxWrapper_guarantee}>
-                <div className={style.content__options_infoBoxWrapper_guarantee_item}>
+              <div className={style.guarantee}>
+                <div className={style.guarantee__item}>
                   <FiCheck color="#33FFCE" />
                   <p>30 day trial</p>
                 </div>
 
-                <div
-                  className={
-                    style.content__options_infoBoxWrapper_guarantee_item
-                  }
-                >
+                <div className={style.guarantee__item}>
                   <FiCheck color="#33FFCE" />
                   <p>Annual contract</p>
                 </div>
-                <div
-                  className={
-                    style.content__options_infoBoxWrapper_guarantee_item
-                  }
-                >
+
+                <div className={style.guarantee__item}>
                   <FiCheck color="#33FFCE" />
                   <p>Full Access to our knowledge base</p>
                 </div>
               </div>
-
               <Paypal />
             </div>
           </div>
