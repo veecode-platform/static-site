62931eb0083fb414c4082c2ba229688c01644447
ValberJunior <valberjr1992@gmail.com>
2022-09-12 15:50:42 -0300

    hotfix

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,11 +1,5 @@
 import style from '../../../styles/_Button.module.scss';
 
-/*const Button = ({children, type, loading}) => {
-  return (
-    <button className={style.button} type={type || "button"} disabled={loading}>
-      {loading ? <><span className={style.loader}></span> loading</> : children}
-    </button>
-  )*/
 const Button = ({color,background,children, loading, type}) => {
   return (
     <>
--- src/screens/checkout/Checkout.jsx
+++ src/screens/checkout/Checkout.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router'
-import { Breadcrumb, Button, Header, Input, Checkbox, Footer} from '../../components';
+import { Breadcrumb, Button, Header, Input, Checkbox, Footer, DefaultPage} from '../../components';
 import style from '../../../styles/_Checkout.module.scss';
 import { Formik, Form} from 'formik';
 import { object, string, boolean} from 'yup';
@@ -25,48 +25,43 @@ const CheckoutPage = () =>{
     //const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
     return(
-        <main>
-            <Link href="/">
-                <a>
-                    <Header/>
-                </a>
-            </Link>
-
+        <DefaultPage 
+            titleBar="disable"
+            >
             <section className={style.wrapper}>
-            <article className={style.content}>
-              <Breadcrumb
-               active={2}
-               />
-              <div className={style.content__options}>
-                <div className={style.content__options_formWraper}>
-                    <Formik
-                        initialValues={{name: "", company: "", email: "", title: "", terms: false }}
-                        validationSchema={formSchema}
-                        onSubmit={async (values)=>{
-                            //await sleep(3000)
-                            const response = await UsePostData(JSON.stringify(values));
-                            console.log(values, response);
-                            await handleFormRedirect();                         
-                        }}
-                    >
-                        {({ errors, touched,/* isValid, validateForm, */handleSubmit, isSubmitting}) => (
-                            
-                            <Form onSubmit={handleSubmit} className={style.content__options_formWraper_form}>
-                                <Input name="name" placeholder="First and last name" label="Full Name" error={(errors.name && touched.name) && errors.name }/>
-                                <Input name="company" placeholder="Acme, Inc." label="Company / Organization" error={(errors.company && touched.company) && errors.company }/>
-                                <Input name="email" placeholder="you@acme.com" label="Business Email" error={(errors.email && touched.email) && errors.email }/>
-                                <Input name="title" placeholder="Sr Engineer" label="Title" error={(errors.title && touched.title) && errors.title }/>
-                                <Checkbox name="terms" error={(errors.terms && touched.terms) && errors.terms}></Checkbox>
-                                <Button type="submit" loading={isSubmitting}>Continue</Button>
-                            </Form>
-                        )} 
-                    </Formik>
-                </div>                 
-              </div>
-          </article>
-      </section>
-      <Footer/>
-    </main>
+                    <article className={style.content}>
+                    <Breadcrumb
+                    active={2}
+                    />
+                    <div className={style.content__options}>
+                        <div className={style.content__options_formWraper}>
+                            <Formik
+                                initialValues={{name: "", company: "", email: "", title: "", terms: false }}
+                                validationSchema={formSchema}
+                                onSubmit={async (values)=>{
+                                    //await sleep(3000)
+                                    const response = await UsePostData(JSON.stringify(values));
+                                    console.log(values, response);
+                                    await handleFormRedirect();                         
+                                }}
+                                >
+                                {({ errors, touched,/* isValid, validateForm, */handleSubmit, isSubmitting}) => (
+                                    
+                                    <Form onSubmit={handleSubmit} className={style.content__options_formWraper_form}>
+                                        <Input name="name" placeholder="First and last name" label="Full Name" error={(errors.name && touched.name) && errors.name }/>
+                                        <Input name="company" placeholder="Acme, Inc." label="Company / Organization" error={(errors.company && touched.company) && errors.company }/>
+                                        <Input name="email" placeholder="you@acme.com" label="Business Email" error={(errors.email && touched.email) && errors.email }/>
+                                        <Input name="title" placeholder="Sr Engineer" label="Title" error={(errors.title && touched.title) && errors.title }/>
+                                        <Checkbox name="terms" error={(errors.terms && touched.terms) && errors.terms}></Checkbox>
+                                        <Button type="submit" loading={isSubmitting}>Continue</Button>
+                                    </Form>
+                                )} 
+                            </Formik>
+                        </div>                 
+                    </div>
+                </article>
+            </section>
+        </DefaultPage>
   );
 };
 
