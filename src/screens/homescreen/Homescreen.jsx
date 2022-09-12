da6a4f356fbd62acda95377b5c6a87e451a4c0d0
ValberJunior <valberjr1992@gmail.com>
2022-09-12 16:29:34 -0300

    hotfix

--- src/screens/homescreen/Homescreen.jsx
+++ src/screens/homescreen/Homescreen.jsx
@@ -1,16 +1,11 @@
-import style from "../../../styles/_Homescreen.module.scss";
+import style from '../../../styles/_Homescreen.module.scss';
 import { Footer, Header } from '../../components';
-import FirstSection from "./firstSection";
-// import Benefits from "./benefits";
-// import OptimizationAndSupport from "./optimizationAndSupport";
-// import ContactSection from "./contactSection";
-import dynamic from "next/dynamic";
-import Banner from "./banner";
-
-const OptimizeYourBusiness = dynamic (()=> import ('./optimizesYourBusiness'));
-const Benefits = dynamic (()=> import ('./benefits'));
-const OptimizationAndSupport = dynamic (()=> import ('./optimizationAndSupport'));
-const SupportSection = dynamic (()=> import ('./supportSection'));
+import FirstSection from './firstSection';
+import Benefits from './benefits';
+import OptimizeYourBusiness from './optimizesYourBusiness';
+import OptimizationAndSupport from './optimizationAndSupport';
+import SupportSection from './supportSection';
+import Banner from './banner';
 
 
 const Homescreen = () => {
