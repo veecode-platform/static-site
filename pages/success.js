e77b6d225f4a9613d92a2234a37505718a05ff1e
ValberJunior <valberjr1992@gmail.com>
2022-09-13 20:45:55 -0300

    create success page

---
+++ pages/success.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import { Success } from '../src/screens'
+
+export default function SuccessPage() {
+  return (
+    <>
+      <Head>
+        <title> Waiting Confirmation</title>
+        <meta name="description" content="Success Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Success/>
+    </>
+  )
+}
