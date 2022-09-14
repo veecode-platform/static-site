f60e0b1001593f09544fb86c37017c5a328d6d61
ValberJunior <valberjr1992@gmail.com>
2022-09-14 10:05:08 -0300

    code review

--- src/screens/failed/Failed.jsx
+++ src/screens/failed/Failed.jsx
@@ -1,44 +1,40 @@
-import { Button, DefaultPage } from '../../components';
-import style from '../../../styles/_Failed.module.scss';
-import Link from 'next/link';
+import { Button, DefaultPage } from "../../components";
+import style from "../../../styles/_Failed.module.scss";
+import Link from "next/link";
 
-const SuccessImage = '/assets/icons/failed.gif'
+const SuccessImage = "/assets/icons/failed.gif";
 
 const Failed = () => {
   return (
     <DefaultPage
        titleBar="disable"
-       noBack
-       >
-        <article className={style.content}>
-              <div className={style.content__image}>
-                  <img
-                      src={SuccessImage} 
-                      alt="Success Animation"
-                    />
-              </div>
-              <div className={style.content__details}>
-                  <p>Failed Operation</p>
-                  <p>There was an error in the transaction, please check your details and try again ...</p>
-              </div>
-              <div className={style.content__buttonWrapper}>
-              <Link
-                href="/checkout"
-                passHref
-              >
-                <a>
-                <Button
-                    color="#FFFFFF"
-                    background="#20a082"
-                  >
-                    Try Again
-                  </Button>
-                </a>
-              </Link>
-            </div>
-        </article>
+       noBack>
+      <article className={style.content}>
+        <div className={style.content__image}>
+          <img
+            src={SuccessImage}
+            alt="Success Animation"
+             />
+        </div>
+        <div className={style.content__details}>
+          <p>Failed Operation</p>
+          <p>
+            There was an error in the transaction, please check your details and
+            try again ...
+          </p>
+        </div>
+        <div className={style.content__buttonWrapper}>
+          <Link href="/checkout" passHref>
+            <a>
+              <Button>
+                Try Again
+              </Button>
+            </a>
+          </Link>
+        </div>
+      </article>
     </DefaultPage>
-  )
-}
+  );
+};
 
-export default Failed
+export default Failed;
--- src/screens/success/Success.jsx
+++ src/screens/success/Success.jsx
@@ -1,44 +1,38 @@
-import { Button, DefaultPage } from '../../components';
-import style from '../../../styles/_Success.module.scss';
-import Link from 'next/link';
+import { Button, DefaultPage } from "../../components";
+import style from "../../../styles/_Success.module.scss";
+import Link from "next/link";
 
-const SuccessImage = '/assets/icons/success.gif'
+const SuccessImage = "/assets/icons/success.gif";
 
 const Success = () => {
   return (
     <DefaultPage
-       titleBar="disable"
-       noBack
-       >
-        <article className={style.content}>
-              <div className={style.content__image}>
-                  <img
-                      src={SuccessImage} 
-                      alt="Success Animation"
-                    />
-              </div>
-              <div className={style.content__details}>
-                  <p>Payment confirmation in progress</p>
-                  <p> We will send you an email when everything is ok ...</p>
-              </div>
-              <div className={style.content__buttonWrapper}>
-              <Link
-                href="/"
-                passHref
-              >
-                <a>
-                <Button
-                    color="#FFFFFF"
-                    background="#20a082"
-                  >
-                    Go Home
-                  </Button>
-                </a>
-              </Link>
-            </div>
-        </article>
+     titleBar="disable"
+     noBack
+     >
+      <article className={style.content}>
+        <div className={style.content__image}>
+          <img 
+            src={SuccessImage} 
+            alt="Success Animation"
+             />
+        </div>
+        <div className={style.content__details}>
+          <p>Payment confirmation in progress</p>
+          <p> We will send you an email when everything is ok ...</p>
+        </div>
+        <div className={style.content__buttonWrapper}>
+          <Link href="/" passHref>
+            <a>
+              <Button>
+                Go Home
+              </Button>
+            </a>
+          </Link>
+        </div>
+      </article>
     </DefaultPage>
-  )
-}
+  );
+};
 
-export default Success
+export default Success;
