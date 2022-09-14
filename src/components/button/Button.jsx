dd31da2fc06b96e114602bcdc33f6c0a6b6c7873
ValberJunior <valberjr1992@gmail.com>
2022-09-14 09:57:39 -0300

    code review

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,8 +2,19 @@ import style from '../../../styles/_Button.module.scss';
 
 const Button = ({children, loading, type, alt}) => {
   return(
-    <button type={type || "button"} disabled={loading} className={style.button} style={alt ? {color: "#FFFFFF", backgroundColor: "#20a082"} : {} }>
-      {loading ? <><span className={style.loader}></span> loading</> : children}
+    <button
+      type={type || "button"}
+      disabled={loading}
+      className={style.button} 
+      style={alt ? {color: "#FFFFFF", backgroundColor: "#20a082"} : {} }
+      >
+      {
+        loading ?
+           <>
+            <span className={style.loader}></span>
+            loading
+          </> : children
+        }
     </button>
   )
 }
