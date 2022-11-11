df53fe65522501f5ab37c8a10cc78d6e5fc7b3b6
ValberJunior <valberjr1992@gmail.com>
2022-11-11 13:31:39 -0300

    add divider component

--- src/screens/homescreen/benefits/index.jsx
+++ src/screens/homescreen/benefits/index.jsx
@@ -1,14 +1,16 @@
 import dynamic from 'next/dynamic'
 import style from './Benefits.module.scss'
 import Data from './benefits.json'
+import Divider from './divider';
 
 const Card = dynamic(()=> import('./card'));
 
 const Benefits = () => {
   return (
     <section className={style.wrapper}>
+      <Divider/>
       <div className={style.title}>
-        <h2>What&apos;s missing from your team?</h2>
+        <h2>What&apos;s missing from <span>your team</span>?</h2>
       </div>
       <article className={style.content}>
         {Data.map((item) => (
