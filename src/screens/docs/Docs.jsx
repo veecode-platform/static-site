c89a7eb60dc0125e010922f1993ea7f1148cdaf5
ValberJunior <valberjr1992@gmail.com>
2022-11-18 18:07:04 -0300

    code review

--- src/screens/docs/Docs.jsx
+++ src/screens/docs/Docs.jsx
@@ -18,7 +18,7 @@ const Docs = () => {
           <p className={style["container__cards-desc"]}>Is a built-in API acceleration tool. The product offers multiple commands that boost productivity and eliminate repetitive tasks.</p>
 
           <Link href="https://docs.platform.vee.codes/docs/installation/safira-cli">
-            <a target="_blank" onClick={gtagEvents('safiraDocs')}>
+            <a target="_blank" onClick={()=>gtagEvents('safiraDocs')}>
               <Button>See Docs</Button>
             </a>
           </Link>
@@ -27,7 +27,7 @@ const Docs = () => {
           <h2 className={style["container__cards-title"]}><span><FaLayerGroup /></span> VKPR</h2>
           <p className={style["container__cards-desc"]}> Is our solution for Cloud Infrastructure. With the CLI, you will provide a Kubernetes cluster to attend to your company&apos;s needs.</p>
           <Link href="https://docs.vkpr.net/docs/intro/">
-            <a target="_blank" onClick={gtagEvents('vkprDocs')}>
+            <a target="_blank" onClick={()=>gtagEvents('vkprDocs')}>
               <Button>See Docs</Button>
             </a>
           </Link>
