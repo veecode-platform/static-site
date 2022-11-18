aba4f0cc773576318ba14c4adcd52767da663684
ValberJunior <valberjr1992@gmail.com>
2022-11-18 16:45:51 -0300

    add new function for analytics

--- src/screens/docs/Docs.jsx
+++ src/screens/docs/Docs.jsx
@@ -3,7 +3,7 @@ import { Button, DefaultPage } from '../../components';
 import style from '../../../styles/Docs.module.scss';
 import { FaLayerGroup, FaRocket } from 'react-icons/fa';
 import Link from 'next/link';
-import { gtagSafiraDocs, gtagVkprDocs } from '../../../utils/gtag';
+import { gtagEvents } from '../../../utils/gtag';
 
 const Docs = () => {
   return (
@@ -18,7 +18,7 @@ const Docs = () => {
           <p className={style["container__cards-desc"]}>Is a built-in API acceleration tool. The product offers multiple commands that boost productivity and eliminate repetitive tasks.</p>
 
           <Link href="https://docs.platform.vee.codes/docs/installation/safira-cli">
-            <a target="_blank" onClick={gtagSafiraDocs}>
+            <a target="_blank" onClick={gtagEvents('safiraDocs')}>
               <Button>See Docs</Button>
             </a>
           </Link>
@@ -27,7 +27,7 @@ const Docs = () => {
           <h2 className={style["container__cards-title"]}><span><FaLayerGroup /></span> VKPR</h2>
           <p className={style["container__cards-desc"]}> Is our solution for Cloud Infrastructure. With the CLI, you will provide a Kubernetes cluster to attend to your company&apos;s needs.</p>
           <Link href="https://docs.vkpr.net/docs/intro/">
-            <a target="_blank" onClick={gtagVkprDocs}>
+            <a target="_blank" onClick={gtagEvents('vkprDocs')}>
               <Button>See Docs</Button>
             </a>
           </Link>
--- utils/gtag.js
+++ utils/gtag.js
@@ -13,24 +13,6 @@ export const gtagPurchase = (id, plan) => {
   window.gtag("event", "purchase", params);
 };
 
-export const gtagSafiraDocs = () => {
-  //create unique event(safira docs redirect)
-  window.gtag("event", "safiraDocs");
-};
-export const gtagVkprDocs = () => {
-  //create unique event(vkpr docs redirect)
-  window.gtag("event", "vkprDocs");
-};
-export const gtagDevportal = () => {
-  //create unique event(Devportal docs redirect)
-  window.gtag("event", "devportal");
-};
-
-export const gtagJoinUs = () => {
-  //create unique event(Devportal docs redirect)
-  window.gtag("event", "joinUs");
-};
-
 export const gtagEvents = (tag) => {
   window.gtag("event", `${tag}`);
 };
