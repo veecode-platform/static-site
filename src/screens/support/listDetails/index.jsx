69381a661925f8ec71f39dfb4b1af6365fa3b144
julopesrocha <jujulrocha@gmail.com>
2022-12-22 14:08:26 -0300

    stack lists

--- src/screens/support/listDetails/index.jsx
+++ src/screens/support/listDetails/index.jsx
@@ -43,6 +43,18 @@ const ListDetails = () => {
                 <li className={style["details__list-item"]}>
                    <span><FaCheck/></span> Code Generator Spring Boot
                 </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> Keycloak
+                </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> Terraform
+                </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> Docker
+                </li>
+                <li className={style["details__list-item"]}>
+                   <span><FaCheck/></span> Argo CD
+                </li>
             </ul>
         </div>
     </article>
