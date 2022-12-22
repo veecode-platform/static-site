b9c31f1184cf0a5ed00bb1ea872cc242792ea22e
julopesrocha <jujulrocha@gmail.com>
2022-12-22 14:25:20 -0300

    compare plans text

--- src/screens/comparePlans/ComparePlans.jsx
+++ src/screens/comparePlans/ComparePlans.jsx
@@ -16,44 +16,34 @@ const ComparePlans = () => {
         {/* STANDARD */}
         <PlansDetails
           title="Standard"
-          subtitle="Rely on our standard support to solve your digital transition
-          problems."
+          subtitle="Solve your company's technological barriers in a practical and objective way."
           link="/validate?plan=standard"
           label="Start at"
-          emphasis="$ 899"
+          emphasis="$ 1990"
           text="per Month"
-          buttonLabel="Know Our Plans"
+          buttonLabel="Sign the plan"
         >
-          <li><span><FaCheck /></span>Professional Support.</li>
-          <li><span><FaCheck /></span>Access to professional specialists</li>
-          <li><span><FaCheck /></span>Up to 10 Users</li>
-          <li><span><FaCheck /></span>3-5 days SLA</li>
-          <li><span><FaCheck /></span>Community Support</li>
+          <li><span><FaCheck /></span>8x5 support</li>
+          <li><span><FaCheck /></span>Attendance from 9h to 18h</li>
+          <li><span><FaCheck /></span>Ticketing tools for service</li>
+          <li><span><FaCheck /></span>10 tickets per month</li>
+          <li><span><FaCheck /></span>2 simultaneous tickets</li>
+          <li><span><FaCheck /></span>One business day SLA</li>
+          <li><span><FaCheck /></span>Direct access to technical professionals</li>
         </PlansDetails>
         {/* ENTERPRISE */}
         <PlansDetails
           title="Enterprise"
-          subtitle="We do it with you and for you. Our enterprise plan covers all your needs with the digital transition with same-day SLA. Ideal for your company."
+          subtitle="Get personalized service according to your company's specific needs."
           link="/contact-enterprise"
-          emphasis="Contact Us"
+          emphasis="Personalize your experience"
           buttonLabel="Contact Us"
         >
-          <li><span><FaCheck /></span>
-            Support for you company needs as:
-            <ul className={style.sublist}>
-              <li><span><FaCheckCircle /></span>Kong</li>
-              <li><span><FaCheckCircle /></span>Kubernetes</li>
-              <li><span><FaCheckCircle /></span>AWS</li>
-              <li><span><FaCheckCircle /></span>Terraform</li>
-              <li><span><FaCheckCircle /></span>IaC and Microservices </li>
-            </ul>
-          </li>
-          <li><span><FaCheck /></span>Unlimited Users</li>
-          <li><span><FaCheck /></span>Direct access to professional specialists</li>
-          <li><span><FaCheck /></span>DevOps Training & Docs</li>
-          <li><span><FaCheck /></span>Enterprise Support</li>
-          <li><span><FaCheck /></span>Prioritized Bug Fixes</li>
-          <li><span><FaCheck /></span>Same business day SLA</li>
+          <li><span><FaCheck /></span>24x7 support</li>
+          <li><span><FaCheck /></span>SLA on the same business day</li>
+          <li><span><FaCheck /></span>Prioritized bug fixes</li>
+          <li><span><FaCheck /></span> Direct access to technical professionals</li>
+          <li><span><FaCheck /></span> DevOps training and documentation</li>
         </PlansDetails>
       </article>
       <article className={style.sectionBar}>
