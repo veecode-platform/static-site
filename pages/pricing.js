8b2f2efc558213fd76376d4c551c6c80819852ef
julopesrocha <jujulrocha@gmail.com>
2022-12-23 10:10:29 -0300

    commented pricing page build

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,17 +1,17 @@
-// import Head from 'next/head'
+import Head from 'next/head'
 // import { Pricing } from '../src/screens'
 
-// export default function PricingPage() {
-//   return (
-//     <>
-//       <Head>
-//         <title>Platform | Pricing</title>
-//         <meta
-//          name="description"
-//          content="Select an optimal plan to get the best out of the platform."
-//          />
-//       </Head>
-//       <Pricing />
-//     </>
-//   )
-// }
+export default function PricingPage() {
+  return (
+    <>
+      <Head>
+        <title>Platform | Pricing</title>
+        <meta
+         name="description"
+         content="Select an optimal plan to get the best out of the platform."
+         />
+      </Head>
+      {/* <Pricing /> */}
+    </>
+  )
+}
