e0b689c8143ca712a330b378542885575421af6e
julopesrocha <jujulrocha@gmail.com>
2023-01-03 18:09:11 -0300

    fix background img desktop

--- src/screens/homescreen/firstSection/index.jsx
+++ src/screens/homescreen/firstSection/index.jsx
@@ -7,7 +7,7 @@ import TextSlide from "./textSlide";
 import { gtagSafiraDocs } from "../../../../utils/gtag";
 
 const Logo = "assets/home/logo.png";
-const BgDesktop = "assets/home/background.png"; 
+const BgDesktop = "assets/home/backy.png"; 
 const BgMobile = "assets/home/background-mobile.png";
 
 const FirstSection = () => {
