6557f90d99eecb9679a8308495dbb190ca086a63
julopesrocha <jujulrocha@gmail.com>
2023-01-05 11:46:22 -0300

    + kong logo support

--- src/screens/homescreen/kongSupport/index.jsx
+++ src/screens/homescreen/kongSupport/index.jsx
@@ -3,13 +3,24 @@ import style from './Kong.module.scss'
 import Data from './kong.json'
 
 const Card = dynamic(()=> import('./card'));
+const kongLogo = "assets/home/text_slide/kong.png"; 
 
 const Kong = () => {
   return (
     <section className={style.wrapper}>
-      <div className={style.title}>
-        <h2>Kong Gateway<span> Support</span></h2>
+      <div className={style.container}>
+        <div>
+          <img 
+            className={style.container__logo}
+            src={kongLogo}
+            alt="Kong Gateway Support Logo - Desktop Version"
+          />
+        </div>
+        <div className={style.container__title}>
+          <h2>Kong Gateway<span> Support</span></h2>
+        </div>
       </div>
+      
       <article className={style.content}>
         {Data.map((item) => (
           <Card key={item.id} {...item} />
