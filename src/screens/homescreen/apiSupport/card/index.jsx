1e74cb57522db415f843cf1dd32b80c3f7a5747c
julopesrocha <jujulrocha@gmail.com>
2023-01-05 15:42:40 -0300

    re built of card + loop to adapt information

--- src/screens/homescreen/apiSupport/card/index.jsx
+++ src/screens/homescreen/apiSupport/card/index.jsx
@@ -1,33 +1,20 @@
 import style from './Card.module.scss'
 
 const Card = ({
-    image,
     title,
-    titleItem1,
-    descItem1,
-    titleItem2,
-    descItem2,
-    titleItem3,
-    descItem3,
+    items,
 }) => {
+    let list =[];
+    items.forEach((item, index) =>{
+        list.push(<li key={index} className={style["card__options-item"]}><h3>{item}</h3></li>);
+    }) 
     return (
         <div className={style.card}>
             <div className={style.card__title}>
                 <h2>{title}</h2>
             </div>
             <ul className={style.card__options}>
-                <li className={style["card__options-item"]}>
-                    <h3>{titleItem1}</h3>
-                    <p>{descItem1}</p>
-                </li>
-                <li className={style["card__options-item"]}>
-                    <h3>{titleItem2}</h3>
-                    <p>{descItem2}</p>
-                </li>
-                <li className={style["card__options-item"]}>
-                    <h3>{titleItem3}</h3>
-                    <p>{descItem3}</p>
-                </li>
+                {list}
             </ul>
         </div>
     );
