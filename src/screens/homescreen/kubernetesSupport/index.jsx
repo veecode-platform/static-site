1d36189b22e3d107147c958ce95c7b1dcb155fdb
julopesrocha <jujulrocha@gmail.com>
2023-01-05 11:53:34 -0300

    + kubernetes logo desktop

--- src/screens/homescreen/kubernetesSupport/index.jsx
+++ src/screens/homescreen/kubernetesSupport/index.jsx
@@ -3,17 +3,22 @@ import style from './Kubernetes.module.scss'
 import Data from './kubernetes.json'
 
 const Card = dynamic(()=> import('./card'));
-// const kongLogo = "assets/home/tools/KONG.png"; 
+const kubernetesLogo = "assets/home/text_slide/kubernetes.png"; 
 
 const Kubernetes = () => {
   return (
     <section className={style.wrapper}>
-      <div className={style.title}>
-      {/* <img
-            src={KubernetesLogo}
-            alt="background header - Desktop Version"
-            /> */}
-        <h2>Kubernetes <span>Support</span></h2>
+      <div className={style.container}>
+        <div>
+          <img 
+            className={style.container__logo}
+            src={kubernetesLogo}
+            alt="Kubernetes Support Logo - Desktop Version"
+          />
+        </div>
+        <div className={style.container__title}>
+          <h2>Kubernetes <span>Support</span></h2>
+        </div>
       </div>
       <article className={style.content}>
         {Data.map((item) => (
