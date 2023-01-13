11d4e6bcb9445128076263bca942685836328a97
julopesrocha <jujulrocha@gmail.com>
2023-01-13 14:29:11 -0300

    + gif animation

--- src/screens/supportPlans/firstSection/index.jsx
+++ src/screens/supportPlans/firstSection/index.jsx
@@ -3,22 +3,24 @@ import style from "./FirstSection.module.scss";
 import { Button } from "../../../components";
 import Link from "next/link";
 import Image from "next/image";
+import Divider from "./divider/index";
 
 const BgDesktop = "/assets/support/bg1.webp";
 const BgMobile = "assets/home/background-mobile.png";
 const Logo = "/assets/home/logo.avif";
-const animation = "/assets/support/platform_animation.gif";
+const animation = "/assets/support/animation_15.gif";
+const static_note = "/assets/support/static_note.png";
 
 const FirstSection = () => {
   return (
     <section className={style.wrapper}>
       {/* Background */}
       <div className={style.wrapper__background}>
-        <img
-          src={BgDesktop}
+        {/* <img
+          src={}
           alt="background header - Desktop Version"
           className={style["wrapper__background-desktop"]}
-        />
+        /> */}
         {/* <img
           src={BgMobile}
           alt="background header - Mobile version"
@@ -55,11 +57,19 @@ const FirstSection = () => {
           <div>
             <img
               src={animation}
+              className={style.animation__img}
             />
           </div>
         </div>
-
+        {/* <div className={style.content__staticNote}>
+          <div>
+            <img
+              src={static_note}
+            />
+          </div>
+        </div> */}
       </article>
+      {/* <Divider /> */}
     </section>
   );
 };
--- src/screens/supportPlans/supportSection/index.jsx
+++ src/screens/supportPlans/supportSection/index.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { Button } from '../../../components';
 import SupportCards from './SupportCards';
 import style from './SupportSection.module.scss';
-
 const SupportSection = ({ title, img, data, buttonLabel, buttonLink }) => {
     return (
         <section className={style.wrapper}>
