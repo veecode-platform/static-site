dd6af4ef0d22b8f52bfc65d2f6ccb04d3ff96c69
luccas <luccasmtxr@gmail.com>
2023-02-15 17:51:26 -0300

    change hotjar script

--- pages/_app.js
+++ pages/_app.js
@@ -26,12 +26,12 @@ function MyApp({ Component, pageProps }) {
       {/*Hotjar Tracking Code for https://platform.vee.codes/ */}
       <Script id="hotjar-script-function">
         {`(function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:3144537,hjsv:6};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3369176,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
       </Script>
       <Head>
