f4ee1704aef029ebae22b6fc978d2c63693c26fd
julopesrocha <jujulrocha@gmail.com>
2023-02-27 15:00:28 -0300

    + text from form

--- src/screens/validate/Validate.jsx
+++ src/screens/validate/Validate.jsx
@@ -1,30 +1,30 @@
 import { useRouter } from 'next/router'
-import { Breadcrumb, Button, Input, Checkbox, DefaultPage} from '../../components';
+import { Breadcrumb, Button, Input, Checkbox, DefaultPage } from '../../components';
 import style from '../../../styles/Validate.module.scss';
-import { Formik, Form} from 'formik';
-import { object, string, boolean} from 'yup';
+import { Formik, Form } from 'formik';
+import { object, string, boolean } from 'yup';
 import { UsePostData } from '../../hooks/UsePostData';
 import { useEffect } from 'react';
 
-const Validate = () =>{
+const Validate = () => {
 
-    useEffect(()=>{
-        if (typeof window !== "undefined"){
-          if ( window.innerWidth > 1300){
-              setTimeout(() => {
-                window.scrollTo({
-                  top: 120,
-                  left: 300,
-                  behavior: 'smooth'
-                });
-              }, 500);
-          }
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            if (window.innerWidth > 1300) {
+                setTimeout(() => {
+                    window.scrollTo({
+                        top: 120,
+                        left: 300,
+                        behavior: 'smooth'
+                    });
+                }, 500);
+            }
         }
-      },[]);
+    }, []);
 
     const formSchema = object({
         name: string().required("*required"),
-        company: string().required("*required") ,
+        company: string().required("*required"),
         email: string().email("*invalid email").required("*required"),
         // cel: string().required("*required"),
         title: string().required("*required"),
@@ -38,47 +38,64 @@ const Validate = () =>{
         await router.push("/checkout")
     }
 
-    return(
-        <DefaultPage 
+    return (
+        <DefaultPage
             titleBar="disable"
             noFooter
             showButton={false}
-            >
+        >
             <section className={style.wrapper}>
-                    <article className={style.content}>
+                <article className={style.container}>
                     <Breadcrumb
-                    active={2}
+                        active={2}
                     />
-                    <div className={style.content__options}>
-                        <div className={style["content__options-formWraper"]}>
-                            <Formik
-                                initialValues={{ name: "", company: "", email: "", title: "", terms: false, plan: plan}}
-                                validationSchema={formSchema}
-                                onSubmit={async (values)=>{
-                                    const response = await UsePostData(values);
-                                    console.log(values, response);
-                                    await handleFormRedirect();                         
-                                }}
+                    <article className={style.content}>
+                        <div className={style.text}>
+                            <div className={style.text__header}>
+                                <h2>15 Days <strong><span>Free Trial</span></strong> of Standard Plan</h2>
+                            </div>
+                            <div className={style.text__resume}>
+                                <p>
+                                    Fill out the form on the side and get access to all the features and our support for a 15-day free trial! <br/><br/>
+                                    We guarantee that your information is secure and that we follow all privacy policies and comply with all applicable regulations and laws. <br/><br/>
+                                    Try our solutions and take advantage of our dedicated support to answer any questions you may have.<br/><br/>
+                                    After submitting the form, you will receive an email with access to our ticketing platform, where you can start asking questions and developing your business right away!<br/><br/>
+                                    Don't waste any more time and sign up for our free 15-day trial of the Standard Plan. Fill out the form to the side now and start enjoying everything we have to offer!
+
+                                </p>
+                            </div>
+                        </div>
+                        <div className={style.content__options}>
+                            <div className={style["content__options-formWraper"]}>
+                                <Formik
+                                    initialValues={{ name: "", company: "", email: "", title: "", terms: false, plan: plan }}
+                                    validationSchema={formSchema}
+                                    onSubmit={async (values) => {
+                                        const response = await UsePostData(values);
+                                        console.log(values, response);
+                                        await handleFormRedirect();
+                                    }}
                                 >
-                                {({ errors, touched, handleSubmit, isSubmitting}) => (
-                                    
-                                    <Form onSubmit={handleSubmit} className={style.form}>
-                                        <Input name="name" placeholder="First and last name" label="Your name" error={(errors.name && touched.name) && errors.name }/>
-                                        <Input name="title" placeholder="Sr Engineer" label="Title" error={(errors.title && touched.title) && errors.title }/>
-                                        <Input name="company" placeholder="Acme, Inc." label="Company / Organization" error={(errors.company && touched.company) && errors.company }/>
-                                        <Input name="email" placeholder="you@acme.com" label="Business Email" error={(errors.email && touched.email) && errors.email }/>
-                                        {/* <Input name="cel" placeholder="number" label="Cel number" error={(errors.cel && touched.cel) && errors.cel }/> */}
-                                        <Checkbox name="terms" terms error={(errors.terms && touched.terms) && errors.terms}/>
-                                        <div className={style.form__buttonWrapper}><Button type="submit" loading={isSubmitting}>Continue</Button></div>
-                                    </Form>
-                                )} 
-                            </Formik>
-                        </div>                 
-                    </div>
+                                    {({ errors, touched, handleSubmit, isSubmitting }) => (
+
+                                        <Form onSubmit={handleSubmit} className={style.form}>
+                                            <Input name="name" placeholder="First and last name" label="Your name" error={(errors.name && touched.name) && errors.name} />
+                                            <Input name="title" placeholder="Sr Engineer" label="Title" error={(errors.title && touched.title) && errors.title} />
+                                            <Input name="company" placeholder="Acme, Inc." label="Company / Organization" error={(errors.company && touched.company) && errors.company} />
+                                            <Input name="email" placeholder="you@acme.com" label="Business Email" error={(errors.email && touched.email) && errors.email} />
+                                            {/* <Input name="cel" placeholder="number" label="Cel number" error={(errors.cel && touched.cel) && errors.cel }/> */}
+                                            <Checkbox name="terms" terms error={(errors.terms && touched.terms) && errors.terms} />
+                                            <div className={style.form__buttonWrapper}><Button type="submit" loading={isSubmitting}>Continue</Button></div>
+                                        </Form>
+                                    )}
+                                </Formik>
+                            </div>
+                        </div>
+                    </article>
                 </article>
             </section>
         </DefaultPage>
-  );
+    );
 };
 
 export default Validate;
