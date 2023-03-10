3ab56f8e7d4e0fe1edf07c7f091afd6bae28d30b
julopesrocha <jujulrocha@gmail.com>
2023-03-09 23:39:54 -0300

    + link 'support' sentence to page

--- src/screens/comparePlans/ComparePlans.jsx
+++ src/screens/comparePlans/ComparePlans.jsx
@@ -3,6 +3,7 @@ import { Breadcrumb, ContactBar, DefaultPage, Guarantees } from "../../component
 import style from "../../../styles/ComparePlans.module.scss";
 import PlansDetails from "./plansDetails";
 import { FaCheck, FaCheckCircle, FaMinus } from "react-icons/fa";
+import Link from "next/link";
 
 const ComparePlans = () => {
   return (
@@ -18,7 +19,8 @@ const ComparePlans = () => {
         {/* STANDARD */}
         <PlansDetails
           title="Standard"
-          subtitle="Start your free trial right now, with no credit card required to sign up!"
+          // subtitle="Start your free trial right now, with no credit card required to sign up!"
+          subtitle="Get access to our platform and support ticket, so you can clear all your doubts about our solutions: click here to learn more. Best of all, our trial does not require you to register your credit card."
           link="/validate?plan=standard"
           label="Start at"
           emphasis="$ 1990"
@@ -26,7 +28,7 @@ const ComparePlans = () => {
           buttonLabel="Free Test"
           banner
         >
-          <li><span><FaCheck /></span>8x5 support</li>
+          <li><span><FaCheck /></span>8x5 <Link href="/support" passHref><a><span>Support</span></a></Link></li>
           <li><span><FaCheck /></span>One business day SLA</li>
           <li><span><FaCheck /></span>10 tickets per month</li>
           <li><span><FaCheck /></span>2 simultaneous tickets</li>
@@ -41,7 +43,7 @@ const ComparePlans = () => {
           buttonLabel="Contact Us"
           banner={false}
         >
-          <li><span><FaCheck /></span>24x7 support</li>
+          <li><span><FaCheck /></span>24x7 <Link href="/support" passHref><a><span>Support</span></a></Link></li>
           <li><span><FaCheck /></span>SLA on the same business day</li>
           <li><span><FaCheck /></span>Prioritized bug fixes</li>
           <li><span><FaCheck /></span> Direct access to technical professionals</li>
