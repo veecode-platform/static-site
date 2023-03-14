53c205eab4c48a249cfb1e8cea58e581dfdf0b94
julopesrocha <jujulrocha@gmail.com>
2023-03-14 14:37:06 -0300

    + basic card plan

--- src/screens/comparePlans/ComparePlans.jsx
+++ src/screens/comparePlans/ComparePlans.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Breadcrumb, ContactBar,DefaultPage, Guarantees } from "../../components";
+import { Breadcrumb, ContactBar, DefaultPage, Guarantees } from "../../components";
 import style from "../../../styles/ComparePlans.module.scss";
 import PlansDetails from "./plansDetails";
 import { FaCheck, FaCheckCircle, FaMinus } from "react-icons/fa";
+import { useTranslation } from 'react-i18next';
 import Link from "next/link";
 
 const ComparePlans = () => {
+  const { t } = useTranslation();
+
   return (
     <DefaultPage
       showButton
@@ -16,35 +19,56 @@ const ComparePlans = () => {
       <Breadcrumb active={1} />
 
       <article className={style.content}>
+        {/* BASIC */}
+        <PlansDetails
+          title={t("Basic")}
+          subtitle={<>{t("Acelerate your business, secure support for our Developer Portal solution. Solve your problems with API automation, Pipelines, and ")} <Link href='/how-it-works' passHref><a style={{ cursor: "pointer", color: "#1C8068", fontWeight: "bold" }}>{t("more")}</a></Link>.</>}
+          link="/validate?plan=basic"
+          label={t("Start at")}
+          emphasis="$ 690"
+          text={t("per Month")}
+          buttonLabel={t("Checkout")}
+        >
+          <li><span><FaCheck /></span>{t('8x5')} <Link href="/support" passHref><a><span>{t('Support')}</span></a></Link></li>
+          <li><span><FaCheck /></span>{t('One business day SLA')}</li>
+          <li><span><FaCheck /></span>{t('Support to TechDocs, CI/CD and Pull Request plugins')}</li>
+          <li><span><FaCheck /></span>{t('Support to 2 project templates')}</li>
+          <li><span><FaCheck /></span>{t('Direct access to our experts')}</li>
+        </PlansDetails>
+
         {/* STANDARD */}
         <PlansDetails
-          title="Standard"
-          subtitle={<>Get access to our ticket platform and clear all your doubts about <Link href='/how-it-works' passHref><a style={{ cursor: "pointer", color: "#33FFCE" }}>our solutions</a></Link>. Our trial does not require you to register your credit card.</>}
+          title={t("Standard")}
+          subtitle={<>{t("Get all your questions answered about our solutions. No more problems with DevOps, API Development, Kubernetes, AWS and")} <Link href='/how-it-works' passHref><a style={{ cursor: "pointer", color: "#1C8068", fontWeight: "bold" }}>{t("more")}</a></Link>.</>}
           link="/validate?plan=standard"
-          label="Start at"
+          label={t("Start at")}
           emphasis="$ 1990"
-          text="per Month"
-          buttonLabel="Free Trial"
+          text={t("per Month")}
+          buttonLabel={t("Checkout")}
         >
-          <li><span><FaCheck /></span>8x5 <Link href="/support" passHref><a><span>Support</span></a></Link></li>
-          <li><span><FaCheck /></span>One business day SLA</li>
-          {/* <li><span><FaCheck /></span>10 tickets per month</li> */}
-          <li><span><FaCheck /></span>2 simultaneous tickets</li>
-          <li><span><FaCheck /></span>Direct access to technical professionals</li>
+          <li><span><FaCheck /></span>{t('8x5')} <Link href="/support" passHref><a><span>{t('Support')}</span></a></Link></li>
+          <li><span><FaCheck /></span>{t('One business day SLA')}</li>
+          <li><span><FaCheck /></span>{t('Support to all plugins')}</li>
+          <li><span><FaCheck /></span>{t('Support to all templates')}</li>
+          <li><span><FaCheck /></span>{t('Direct access to our experts')}</li>
         </PlansDetails>
+
+
+
         {/* ENTERPRISE */}
         <PlansDetails
-          title="Enterprise"
-          subtitle="Get personalized service according to your company's specific needs"
+          title={t("Enterprise")}
+          subtitle={t("Bring us your problem, and we will bring you the solution. This plan includes all our solutions in a customized way to your demand!")}
           link="/contact-enterprise"
-          headline="Personalize your experience"
-          buttonLabel="Contact Us"
+          headline={t('Personalize your experience')}
+          buttonLabel={t('Contact Us')}
         >
-          <li><span><FaCheck /></span>24x7 <Link href="/support" passHref><a><span>Support</span></a></Link></li>
-          <li><span><FaCheck /></span>SLA on the same business day</li>
-          <li><span><FaCheck /></span>Prioritized bug fixes</li>
-          <li><span><FaCheck /></span> Direct access to technical professionals</li>
-          <li><span><FaCheck /></span> DevOps training and documentation</li>
+          <li><span><FaCheck /></span>{t('24x7')} <Link href="/support" passHref><a><span>{t('Support')}</span></a></Link></li>
+          <li><span><FaCheck /></span>{t('SLA on the same business day')}</li>
+          <li><span><FaCheck /></span>{t('Support to all plugins')}</li>
+          <li><span><FaCheck /></span> {t('Support to all templates')}</li>
+          <li><span><FaCheck /></span> {t('Direct access to our experts')}</li>
+          <li><span><FaCheck /></span> {t('DevOps training and documentation')}</li>
         </PlansDetails>
       </article>
       <article className={style.sectionBar}>
--- src/screens/supportPlans/plans/index.jsx
+++ src/screens/supportPlans/plans/index.jsx
@@ -37,54 +37,55 @@ const Plans = () => {
 
 
             <article className={style.plans}>
-            {/* BASIC */}
-            <PlansDetails
+                {/* BASIC */}
+                <PlansDetails
                     title={t("Basic")}
-                    subtitle={<>{t("Get access to our ticket platform and clear all your doubts about")} <Link href='/how-it-works' passHref><a  style={{ cursor: "pointer", color: "#33FFCE" }}>{t("our solutions")}</a></Link>. {t("Our trial does not require you to register your credit card")}.</>}
-                    link="/validate?plan=standard"
+                    subtitle={<>{t("Acelerate your business now, secure support for our Developer Portal solution. Solve your problems with API automation, Pipelines, and ")} <Link href='/how-it-works' passHref><a style={{ cursor: "pointer", color: "#1C8068", fontWeight: "bold" }}>{t("more")}</a></Link>.</>}
+                    link="/validate?plan=basic"
                     label={t("Start at")}
-                    emphasis="$ 1990"
+                    emphasis="$ 690"
                     text={t("per Month")}
-                    buttonLabel={t("Free Test")}
+                    buttonLabel={t("Checkout")}
                 >
-                    <li><span><FaCheck /></span>{t('8x5 support')}</li>
+                    <li><span><FaCheck /></span>{t('8x5')} <Link href="/support" passHref><a><span>{t('Support')}</span></a></Link></li>
                     <li><span><FaCheck /></span>{t('One business day SLA')}</li>
-                    <li><span><FaCheck /></span>{t('10 tickets per month')}</li>
-                    <li><span><FaCheck /></span>{t('2 simultaneous tickets')}</li>
-                    <li><span><FaCheck /></span>{t('Direct access to technical professionals')}</li>
+                    <li><span><FaCheck /></span>{t('Support to TechDocs, CI/CD and Pull Request plugins')}</li>
+                    <li><span><FaCheck /></span>{t('Support to 2 project templates')}</li>
+                    <li><span><FaCheck /></span>{t('Direct access to our experts')}</li>
                 </PlansDetails>
 
                 {/* STANDARD */}
                 <PlansDetails
                     title={t("Standard")}
-                    subtitle={<>{t("Get access to our ticket platform and clear all your doubts about")} <Link href='/how-it-works' passHref><a style={{ cursor: "pointer", color: "#33FFCE" }}>{t("our solutions")}</a></Link>. {t("Our trial does not require you to register your credit card")}.</>}
+                    subtitle={<>{t("Get all your questions answered about our solutions. No more problems with DevOps, API Development, Kubernetes, AWS and")} <Link href='/how-it-works' passHref><a style={{ cursor: "pointer", color: "#1C8068", fontWeight: "bold" }}>{t("more")}</a></Link>.</>}
                     link="/validate?plan=standard"
                     label={t("Start at")}
                     emphasis="$ 1990"
                     text={t("per Month")}
-                    buttonLabel={t("Free Trial")}
+                    buttonLabel={t("Checkout")}
                 >
                     <li><span><FaCheck /></span>{t('8x5')} <Link href="/support" passHref><a><span>{t('Support')}</span></a></Link></li>
                     <li><span><FaCheck /></span>{t('One business day SLA')}</li>
-                    {/* <li><span><FaCheck /></span>{t('10 tickets per month')}</li> */}
-                    <li><span><FaCheck /></span>{t('2 simultaneous tickets')}</li>
-                    <li><span><FaCheck /></span>{t('Direct access to technical professionals')}</li>
+                    <li><span><FaCheck /></span>{t('Support to all plugins')}</li>
+                    <li><span><FaCheck /></span>{t('Support to all templates')}</li>
+                    <li><span><FaCheck /></span>{t('Direct access to our experts')}</li>
                 </PlansDetails>
 
-                
+
 
                 {/* ENTERPRISE */}
                 <PlansDetails
                     title={t("Enterprise")}
-                    subtitle={t("Get personalized service according to your company's specific needs")}
+                    subtitle={t("Bring us your problem, and we will bring you the solution. This plan includes all our solutions in a customized way to your demand!")}
                     link="/contact-enterprise"
                     headline={t('Personalize your experience')}
                     buttonLabel={t('Contact Us')}
                 >
                     <li><span><FaCheck /></span>{t('24x7')} <Link href="/support" passHref><a><span>{t('Support')}</span></a></Link></li>
                     <li><span><FaCheck /></span>{t('SLA on the same business day')}</li>
-                    <li><span><FaCheck /></span>{t('Prioritized bug fixes')}</li>
-                    <li><span><FaCheck /></span> {t('Direct access to technical professionals')}</li>
+                    <li><span><FaCheck /></span>{t('Support to all plugins')}</li>
+                    <li><span><FaCheck /></span> {t('Support to all templates')}</li>
+                    <li><span><FaCheck /></span> {t('Direct access to our experts')}</li>
                     <li><span><FaCheck /></span> {t('DevOps training and documentation')}</li>
                 </PlansDetails>
             </article>
