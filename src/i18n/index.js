ec0237ba2ca55fee565bab1b07b791e6b36514fb
julopesrocha <jujulrocha@gmail.com>
2023-05-30 17:20:14 -0300

    + i18n updated configs

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,28 +1,28 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import PTLP from './locales/pt/support-plans.json';
-import ENLP from './locales/en/suppot-plans.json';
+import PT from './locales/pt/support-plans.json';
+import EN from './locales/en/suppot-plans.json';
 import detector from "i18next-browser-languagedetector";
 
 const resources = {
-    'pt': PTLP,
-    'en': ENLP,
+    'pt': PT,
+    'en': EN,
 }
 
 i18n
     .use(detector)
     .use(initReactI18next)
     .init({
-    resources,
-    fallbackLng: "en",
-    supportedLngs:['pt', 'en'],
-    lng : i18n.language,
-    ns: 'translation',
-    useBrowserDefault: true,
-    interpolation: {
-        escapeValue:false
-    }
-})
+        resources,
+        fallbackLng: "en",
+        supportedLngs: ['en', 'pt'],
+        lng: ['en', 'pt'],
+        ns: 'translation',
+        useBrowserDefault: true,
+        interpolation: {
+            escapeValue: false
+        }
+    })
 
 
 export default i18n;
