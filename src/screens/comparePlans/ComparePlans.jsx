35ce3596d8bb88ae6ba20db30e628a200a22b194
julopesrocha <jujulrocha@gmail.com>
2023-06-06 18:17:52 -0300

    + compare plans translation

--- src/screens/comparePlans/ComparePlans.jsx
+++ src/screens/comparePlans/ComparePlans.jsx
@@ -3,86 +3,86 @@ import { Breadcrumb, ContactBar, DefaultPage, Guarantees } from "../../component
 import style from "../../../styles/ComparePlans.module.scss";
 import PlansDetails from "./plansDetails";
 import { FaCheck, FaCheckCircle, FaMinus } from "react-icons/fa";
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import Link from "next/link";
 
 const ComparePlans = () => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <DefaultPage
       showButton
       titleBar="enable"
-      title="Chose our support for you"
-      subtitle="No more stress with the digital transition, choose the support that most fits your need:"
+      title={t("pricing-title")}
+      subtitle={t("pricing-subtitle")}
     >
       <Breadcrumb active={1} />
 
       <article className={style.content} id="plans">
         {/* FREE */}
         <PlansDetails
-          title="FREE"
-          subtitle={<>Accelerate your business with our Developer Portal solution. Streamline your API automation, Pipelines, and more to solve your business problems.</>}
-          label="Start now your"
-          emphasis="FREE PLAN"
-          text="and take your business to the next level!"
+          title={t("pricing-plan1-title")}
+          subtitle={<>{t("pricing-plan1-subtitle")}</>}
+          label={t("pricing-plan1-desc1")}
+          emphasis={t("pricing-plan1-desc2")}
+          text={t("pricing-plan1-desc3")}
           link="/free-plan?plan=free"
-          buttonLabel="Start Now"
+          buttonLabel={t("pricing-plan1-label")}
         >
-          <li><span><FaCheck /></span><Link href="https://docs.platform.vee.codes/" passHref><a target={"_blank"}><span>Community Support</span></a></Link></li>
-          <li><span><FaCheck /></span>Access to documentation</li>
+          <li><span><FaCheck /></span><Link href="https://docs.platform.vee.codes/" passHref><a target={"_blank"}><span>{t("pricing-plan1-topic1")}</span></a></Link></li>
+          <li><span><FaCheck /></span>{t("pricing-plan1-topic2")}</li>
         </PlansDetails>
 
         {/* BASIC */}
         <PlansDetails
-          title="Basic"
-          subtitle={<>Acelerate your business, secure support for our Developer Portal solution. Solve your problems with API automation, Pipelines, and <Link href='/how-it-works' passHref><a style={{ cursor: "pointer", color: "#1C8068", fontWeight: "bold" }}>more</a></Link>.</>}
+          title={t("pricing-plan2-title")}
+          subtitle={<>{t("pricing-plan2-subtitle1")} <Link href='/how-it-works' passHref><a style={{ cursor: "pointer", color: "#1C8068", fontWeight: "bold" }}>{t("pricing-plan2-subtitle2")}</a></Link>.</>}
           link="/validate?plan=basic"
-          label="Start at"
+          label={t("pricing-plan2-desc1")}
           emphasis="$ 690"
-          text="per Month"
-          buttonLabel="Checkout"
+          text={t("pricing-plan2-desc3")}
+          buttonLabel={t("pricing-plan2-label")}
         >
-          <li><span><FaCheck /></span>8x5 <Link href="/support" passHref><a><span>Support</span></a></Link></li>
-          <li><span><FaCheck /></span>One business day SLA</li>
-          <li><span><FaCheck /></span>Support to TechDocs, CI/CD and Pull Request plugins</li>
-          <li><span><FaCheck /></span>Support to 2 project templates</li>
-          <li><span><FaCheck /></span>Direct access to our experts</li>
+          <li><span><FaCheck /></span>{t("pricing-plan2-topic1")} <Link href="/support" passHref><a><span>{t("pricing-plan2-topic1.1")}</span></a></Link></li>
+          <li><span><FaCheck /></span>{t("pricing-plan2-topic2")}</li>
+          <li><span><FaCheck /></span>{t("pricing-plan2-topic3")}</li>
+          <li><span><FaCheck /></span>{t("pricing-plan2-topic4")}</li>
+          <li><span><FaCheck /></span>{t("pricing-plan2-topic5")}</li>
         </PlansDetails>
 
         {/* STANDARD */}
         <PlansDetails
-          title="Standard"
-          subtitle={<>Get all your questions answered about our solutions. No more problems with DevOps, API Development, Kubernetes, AWS and <Link href='/how-it-works' passHref><a style={{ cursor: "pointer", color: "#1C8068", fontWeight: "bold" }}>more</a></Link>.</>}
+          title={t("pricing-plan3-title")}
+          subtitle={<>{t("pricing-plan3-subtitle1")} <Link href='/how-it-works' passHref><a style={{ cursor: "pointer", color: "#1C8068", fontWeight: "bold" }}>{t("pricing-plan3-subtitle2")}</a></Link>.</>}
           link="/validate?plan=standard"
-          label="Start at"
+          label={t("pricing-plan3-desc1")}
           emphasis="$ 1990"
-          text="per Month"
-          buttonLabel="Checkout"
+          text={t("pricing-plan3-desc3")}
+          buttonLabel={t("pricing-plan3-label")}
         >
-          <li><span><FaCheck /></span>8x5 <Link href="/support" passHref><a><span>Support</span></a></Link></li>
-          <li><span><FaCheck /></span>One business day SLA</li>
-          <li><span><FaCheck /></span>Support to all plugins</li>
-          <li><span><FaCheck /></span>Support to all templates</li>
-          <li><span><FaCheck /></span>Direct access to our experts</li>
+          <li><span><FaCheck /></span>{t("pricing-plan3-topic1")} <Link href="/support" passHref><a><span>{t("pricing-plan3-topic1.1")}</span></a></Link></li>
+          <li><span><FaCheck /></span>{t("pricing-plan3-topic2")}</li>
+          <li><span><FaCheck /></span>{t("pricing-plan3-topic3")}</li>
+          <li><span><FaCheck /></span>{t("pricing-plan3-topic4")}</li>
+          <li><span><FaCheck /></span>{t("pricing-plan3-topic5")}</li>
         </PlansDetails>
 
 
 
         {/* ENTERPRISE */}
         <PlansDetails
-          title="Enterprise"
-          subtitle="Bring us your problem, and we will bring you the solution. This plan includes all our solutions in a customized way to your demand!"
+          title={t("pricing-plan4-title")}
+          subtitle={t("pricing-plan4-subtitle")}
           link="/contact-enterprise"
-          headline='Personalize your experience'
-          buttonLabel='Contact Us'
+          headline={t("pricing-plan4-desc")}
+          buttonLabel={t("contact-us")}
         >
-          <li><span><FaCheck /></span>24x7 <Link href="/support" passHref><a><span>Support</span></a></Link></li>
-          <li><span><FaCheck /></span>SLA on the same business day</li>
-          <li><span><FaCheck /></span>Support to all plugins</li>
-          <li><span><FaCheck /></span> Support to all templates</li>
-          <li><span><FaCheck /></span> Direct access to our experts</li>
-          <li><span><FaCheck /></span> DevOps training and documentation</li>
+          <li><span><FaCheck /></span>{t("pricing-plan4-topic1")} <Link href="/support" passHref><a><span>{t("pricing-plan4-topic1.1")}</span></a></Link></li>
+          <li><span><FaCheck /></span>{t("pricing-plan4-topic2")}</li>
+          <li><span><FaCheck /></span>{t("pricing-plan4-topic3")}</li>
+          <li><span><FaCheck /></span>{t("pricing-plan4-topic4")}</li>
+          <li><span><FaCheck /></span>{t("pricing-plan4-topic5")}</li>
+          <li><span><FaCheck /></span>{t("pricing-plan4-topic6")}</li>
         </PlansDetails>
       </article>
       <article className={style.sectionBar}>
