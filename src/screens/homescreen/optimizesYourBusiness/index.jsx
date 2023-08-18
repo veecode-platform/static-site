5e9c71ce56d97ab2ab67bf09e68b372b0bbebb1c
ValberJunior <valberjr1992@gmail.com>
2023-08-18 11:36:15 -0300

    update section -- release with new copy

--- src/screens/homescreen/optimizesYourBusiness/index.jsx
+++ src/screens/homescreen/optimizesYourBusiness/index.jsx
@@ -1,7 +1,4 @@
 import style from "./OptimizeYourBusiness.module.scss";
-import { Button } from "../../../components";
-// import Link from "next/link";
-// import Image from "next/image";
 import { useTranslation } from 'react-i18next';
 
 // const Icon = "/assets/support/animation.svg";
@@ -27,24 +24,11 @@ const OptimizeYourBusiness = () => {
               <p>
                 {t("home-optimize-subtitle1")}
                 <strong> {t("home-optimize-subtitle2")}</strong>
-                {t("home-optimize-subtitle3")} 
+                {t("home-optimize-subtitle3")}
                 <strong>{t("home-optimize-subtitle4")}</strong>
-                {t("home-optimize-subtitle5")} 
+                {t("home-optimize-subtitle5")}
               </p>
             </div>
-            {/* <div className={style.details_buttonWrapper}> */}
-            {/* <Link href="https://docs.platform.vee.codes/" passHref>
-                <a target="_blank">
-                  <Button>{t("home-optimize-button-label")}</Button>
-                </a>
-              </Link> */}
-
-            {/* <Link href="/docs" passHref> 
-                <a className={style.button}>
-                  <Button>Start For Free</Button>
-                </a>
-              </Link> */}
-            {/* </div> */}
           </div>
         </div>
         <div className={style["content_contentWrapper-image"]}>
