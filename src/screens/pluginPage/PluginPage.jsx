f77577c1c2bfb9a8d23ef9ca532a11d4eca64e76
ValberJunior <valberjr1992@gmail.com>
2023-10-13 12:01:54 -0300

    fix code block component

--- src/screens/pluginPage/PluginPage.jsx
+++ src/screens/pluginPage/PluginPage.jsx
@@ -7,102 +7,105 @@ import { DefaultPage, ScrollTop } from '../../components';
 import convertUrlToRaw from '../../../utils/convertUrlToRaw';
 import PluginsData from '../../../data/plugins/plugins.json';
 import Link from 'next/link';
-import { CodeBlock, vs2015 } from "react-code-blocks";
+// import { CodeBlock, vs2015 } from "react-code-blocks";
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vs2015 } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+
 
 const PluginPage = ({ title }) => {
 
-    const [titleData, setTitleData] = useState(title);
-    const [content, setContent] = useState('');
-    const plugins = PluginsData.en;
+  const [titleData, setTitleData] = useState(title);
+  const [content, setContent] = useState('');
+  const plugins = PluginsData.en;
 
-    useEffect(() => {
-      if(title){
-        localStorage.setItem('pageTitle', title);
-        setTitleData(title)
+  useEffect(() => {
+    if (title) {
+      localStorage.setItem('pageTitle', title);
+      setTitleData(title)
+    }
+    else {
+      const storedTitle = localStorage.getItem('pageTitle');
+      if (storedTitle) {
+        setTitleData(storedTitle)
       }
-      else{
-        const storedTitle = localStorage.getItem('pageTitle');
-        if(storedTitle){
-          setTitleData(storedTitle)
+    }
+
+    plugins.filter(p => {
+      if (p.title == titleData) {
+        const rawUrl = convertUrlToRaw(p.url);
+        if (rawUrl) {
+          fetch(rawUrl)
+            .then((response) => response.text())
+            .then((data) => {
+              setContent(data);
+            })
+            .catch((error) => {
+              console.error('Error:', error);
+            });
         }
       }
-  
-      plugins.filter(p => {
-        if (p.title == titleData) {
-          const rawUrl = convertUrlToRaw(p.url);
-          if (rawUrl) {
-            fetch(rawUrl)
-              .then((response) => response.text())
-              .then((data) => {
-                setContent(data);
-              })
-              .catch((error) => {
-                console.error('Error:', error);
-              });
-          }
-        }
-      })
-    }, [title]);
+    })
+  }, [title]);
 
-    
-    const CodeComponent = ({ className, children }) => {
-        let language = className ? className.replace(/^language-/, '') : null;
-    
-        if (!language) return <code className={style.marked}>{children}</code>;
-    
-        if (typeof window !== 'undefined') {
-          
-          return (
-            <CodeBlock 
-              text={children} 
-              language={language} 
-              showLineNumbers={false} 
-              theme={vs2015} 
-              className={style.codebox}
-              />
-          );
-        }
-    
-        return null; 
-      };
 
-      const LinkTag = ({href, children}) => {
+  const CodeComponent = ({ className, children }) => {
+    let language = className ? className.replace(/^language-/, '') : null;
 
-        const label = children.join(' ');
+    if (!language) return <code className={style.marked}>{children}</code>;
 
-        return(
-            <Link 
-              href={href}
-              >
-                <a 
-                  target='_blank'
-                  className={style.link}>
-                    {label}
-                </a>
-            </Link>
-        )
-      }
+    if (typeof window !== 'undefined') {
+
+      return (
+        <SyntaxHighlighter
+          language={language}
+          style={vs2015}
+          className={style.codebox}
+        >
+          {children}
+        </SyntaxHighlighter>
+      );
+    }
+
+    return null;
+  };
+
+  const LinkTag = ({ href, children }) => {
+
+    const label = children.join(' ');
 
     return (
-        <DefaultPage notTranslate>
-          <section className={style.wrapper}>
-              <Remark
-                  remarkPlugins={[remarkGemoji]}
-                  remarkToRehypeOptions={{ allowDangerousHtml: true }}
-                  rehypePlugins={[rehypeSlug]}
-                  rehypeReactOptions={{
-                      components: {
-                          code: (props) => <CodeComponent {...props} />,
-                          a: (props) => <LinkTag {...props}/>
-                      },
-                  }}
-               >
-                  {content}
-              </Remark>
-          </section>
-          <ScrollTop />
-        </DefaultPage>
+      <Link
+        href={href}
+      >
+        <a
+          target='_blank'
+          className={style.link}>
+          {label}
+        </a>
+      </Link>
     )
+  }
+
+  return (
+    <DefaultPage notTranslate>
+      <section className={style.wrapper}>
+        <Remark
+          remarkPlugins={[remarkGemoji]}
+          remarkToRehypeOptions={{ allowDangerousHtml: true }}
+          rehypePlugins={[rehypeSlug]}
+          rehypeReactOptions={{
+            components: {
+              code: (props) => <CodeComponent {...props} />,
+              a: (props) => <LinkTag {...props} />
+            },
+          }}
+        >
+          {content}
+        </Remark>
+      </section>
+      <ScrollTop />
+    </DefaultPage>
+  )
 }
 
 export default PluginPage
