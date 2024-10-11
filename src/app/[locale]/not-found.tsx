84fde7c93ffef0f6211f2976d6f138b23b5db676
ValberJunior <valberjr1992@gmail.com>
2024-10-11 14:35:12 -0300

    feat: ✨ add not found page

---
+++ src/app/[locale]/not-found.tsx
@@ -0,0 +1,11 @@
+import { PageNotFound } from "@/screens";
+
+export default function NotFound() {
+  return (
+    <html lang="en">
+      <body>
+        <PageNotFound />
+      </body>
+    </html>
+  );
+}
