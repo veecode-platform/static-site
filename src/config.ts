940e75b0fe04de851d9658cc3be57c9ff47c32db
ValberJunior <valberjr1992@gmail.com>
2024-10-11 11:46:38 -0300

    refactor: 📦

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const port = process.env.PORT || 3000;
 export const host = process.env.NEXT_PUBLIC_HOST_URL
-  ? `${process.env.NEXT_PUBLIC_HOST_URL}`
+  ? process.env.NEXT_PUBLIC_HOST_URL
   : `http://localhost:${port}`;
