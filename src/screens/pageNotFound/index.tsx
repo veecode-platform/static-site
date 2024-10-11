ceeb97821d7c249cf3e69616029dda7048d03a30
ValberJunior <valberjr1992@gmail.com>
2024-10-11 11:15:42 -0300

    style: 💎

--- src/screens/pageNotFound/index.tsx
+++ src/screens/pageNotFound/index.tsx
@@ -1,3 +1,4 @@
+import { Container } from "@/components";
 import style from "./PageNotFoundStyles.module.scss";
 import Link from "next/link";
 import { GoHome } from "react-icons/go";
@@ -7,14 +8,16 @@ const ImageNotFound = "/assets/notFound/404.png";
 export const PageNotFound = () => {
   return (
     <article className={style.root}>
-      <div className={style.content}>
-        <img src={ImageNotFound} alt="Page Not Found Image" />
-        <div className={style.content__goBack}>
-          <Link href="/en/">
-            <GoHome title="Home" />
-          </Link>
+      <Container>
+        <div className={style.content}>
+          <img src={ImageNotFound} alt="Page Not Found Image" />
+          <div className={style.content__goBack}>
+            <Link href="/en/">
+              <GoHome title="Home" />
+            </Link>
+          </div>
         </div>
-      </div>
+      </Container>
     </article>
   );
 };
