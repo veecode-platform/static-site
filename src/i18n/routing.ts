e03ad5c2af38ca6b8934c983330fb420b4faffb9
ValberJunior <valberjr1992@gmail.com>
2025-04-09 16:31:48 -0300

    refactor: 📦

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -11,74 +11,23 @@ export const routing = defineRouting({
     '/plugins': '/plugins',
     '/plugin': '/plugin',
     '/plugin/[path]': '/plugin/[path]',
-    '/pricing': {
-      pt: '/planos',
-      en: '/pricing',
-    },
-    '/pricing/#plans': {
-      pt: '/planos/#plans',
-      en: '/pricing/#plans',
-    },
-    '/free-plan': {
-      pt: '/plano-gratis',
-      en: '/free-plan',
-    },
-    '/free-plan?plan=free': {
-      pt: '/plano-gratis?plan=free',
-      en: '/free-plan?plan=free',
-    },
-    '/contact-us': {
-      pt: '/fale-conosco',
-      en: '/contact-us',
-    },
-    '/contact-success': {
-      pt: '/contato-recebido',
-      en: '/contact-success',
-    },
-    '/contact-failed': {
-      pt: '/contato-falhado',
-      en: '/contact-failed',
-    },
-    '/how-it-works': {
-      pt: '/como-funciona',
-      en: '/how-it-works',
-    },
-    '/support': {
-      pt: '/suporte',
-      en: '/support',
-    },
-    '/terms-of-service': {
-      pt: '/termos-de-servico',
-      en: '/terms-of-service',
-    },
-    '/privacy-policy': {
-      pt: '/politica-de-privacidade',
-      en: '/privacy-policy',
-    },
-    '/faq': {
-      pt: '/perguntas-frequentes',
-      en: '/faq',
-    },
-    '/success': {
-      pt: '/tudo-certo',
-      en: '/success',
-    },
-    '/failed': {
-      pt: '/houve-um-erro',
-      en: '/failed',
-    },
-    '/validate': {
-      pt: '/validacao',
-      en: '/validate',
-    },
-    '/validate?plan=standard': {
-      pt: '/validacao?plan=standard',
-      en: '/validate?plan=standard',
-    },
-    '/governance': {
-      pt: '/governancia',
-      en: '/governance',
-    },
+    '/pricing': '/pricing',
+    '/pricing/#plans': '/pricing/#plans',
+    '/free-plan': '/free-plan',
+    '/free-plan?plan=free': 'free-plan?plan=free',
+    '/contact-us': '/contact-us',
+    '/contact-success': '/contact-success',
+    '/contact-failed': '/contact-failed',
+    '/how-it-works': '/how-it-works',
+    '/support': '/support',
+    '/terms-of-service': '/terms-of-service',
+    '/privacy-policy':'/privacy-policy',
+    '/faq': '/faq',
+    '/success': '/success',
+    '/failed': '/failed',
+    '/validate': '/validate',
+    '/validate?plan=standard': '/validate?plan=standard',
+    '/governance': '/governance',
   },
 });
 
