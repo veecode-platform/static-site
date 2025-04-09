458f8d70e407a5128ac09c5f4c264873d1bfba75
ValberJunior <valberjr1992@gmail.com>
2025-04-09 17:37:12 -0300

    chore: ♻️

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig = {
   output: "export", // static,
   trailingSlash: true,
+  basePath: '/static-site', 
   logging: {
     fetches: {
       hmrRefreshes: true,
