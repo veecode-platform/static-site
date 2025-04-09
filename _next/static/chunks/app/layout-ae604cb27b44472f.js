9dc77110bca110b2a7eb678a9f57bd6b60fa4fd4
ValberJunior <ValberJunior@users.noreply.github.com>
2025-04-09 19:05:22 +0000

    deploy: 47b4cf50e4e08ce7feb73e79b487602258ba4d4f

---
+++ _next/static/chunks/app/layout-ae604cb27b44472f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3185],{96673:function(n,e,u){Promise.resolve().then(u.t.bind(u,10257,23))},10257:function(){}},function(n){n.O(0,[6946,7610,2971,2117,1744],function(){return n(n.s=96673)}),_N_E=n.O()}]);
