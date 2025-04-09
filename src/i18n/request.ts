91f06c18d00c53a9f3dc3736bf6c22e569426eac
ValberJunior <valberjr1992@gmail.com>
2025-04-08 23:35:55 -0300

    refactor: 📦

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,16 +2,22 @@ import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
 
-export default getRequestConfig(async ({ locale }) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!routing.locales.includes(locale as any)) notFound();
+export default getRequestConfig(async ({ requestLocale }) => {
+  // This typically corresponds to the `[locale]` segment
+  let locale = await requestLocale;
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale;
+  }
 
+  // return {
+  //   messages: (
+  //     await (locale === "en"
+  //       ? // When using Turbopack, this will enable HMR for `en`
+  //         import("../../locales/en.json")
+  //       : import(`../../locales/${locale}.json`))
+  //   ).default,
+  // };
   return {
-    messages: (
-      await (locale === "en"
-        ? // When using Turbopack, this will enable HMR for `en`
-          import("../../locales/en.json")
-        : import(`../../locales/${locale}.json`))
-    ).default,
+    messages: (await import(`../../locales/${locale}.json`)).default,
   };
 });
