4254368681e13088c9800f97f1c8c8e1e8832b8b
ValberJunior <valberjr1992@gmail.com>
2025-04-09 15:33:26 -0300

    refactor: 📦

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "standalone",   // dynamic
   output: "export", // static,
   trailingSlash: true,
   logging: {
